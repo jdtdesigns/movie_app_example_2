7aeea43741beafba2f1b839b431f933f34490b8f
JD Tadlock <jt7903@gmail.com>
2022-12-16 09:25:29 -0500

    standard api version completed

--- js/app.js
+++ js/app.js
@@ -4,15 +4,21 @@ var itemWrapper = document.querySelector('main');
 function displayMatches(matches) {
   itemWrapper.innerHTML = '';
 
-  for (var matchObj of matches) {
-    itemWrapper.insertAdjacentHTML('beforeend', `
+  if (!matches) {
+    itemWrapper.innerHTML = '<p class="no-search">No results found.</p>';
+  } else {
+    for (var matchObj of matches) {
+      itemWrapper.insertAdjacentHTML('beforeend', `
       <div class="movie-item" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${matchObj.Poster})">
         <h3>${matchObj.Title}</h3>
         <p>Release Year: ${matchObj.Year}</p>
         <a href="https://www.imdb.com/title/${matchObj.imdbID}" target="_blank">View More Details</a>
       </div>
     `);
+    }
   }
+
+
 }
 
 function getMovieData(event) {
